9184132284f9ab889a3aa0d8be5d18c3c065c94c
Laibawaqar123 <laibawaqar72@gmail.com>
2024-08-20 22:11:04 +0500

    User.css delete, Profile form change

--- src/components/Dashboard/Profile.jsx
+++ src/components/Dashboard/Profile.jsx
@@ -95,41 +95,7 @@ export const Profile = () => {
                   className="text-sm text-red-600 mt-1"
                 />
               </div>
-
-              {/* <div className="relative z-0 w-full group">
-                <Field
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="block py-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="email"
-                  className="peer-focus:font-medium absolute text-sm italic text-blue-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:text-blue-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Email Address
-                </label>
-                <ErrorMessage name="email" component="div" className="text-red-600" />
-              </div>
-
-              <div className="relative z-0 w-full group">
-                <Field
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="block py-2 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="password"
-                  className="peer-focus:font-medium absolute text-sm italic text-blue-600 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 peer-focus:text-blue-900 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Password
-                </label>
-                <ErrorMessage name="password" component="div" className="text-red-600" />
-              </div> */}
-
+              
               <div className="relative z-0 w-full group">
                 <Field
                   type="text"
