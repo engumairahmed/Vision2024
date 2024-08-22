9ea49d93b60520f1a9497542b8163a2db9b9cf87
engumairahmed <eng.umair.a@gmail.com>
2024-08-22 16:08:43 +0500

    Update ProductManagement.jsx

--- src/components/Dashboard/ProductManagement.jsx
+++ src/components/Dashboard/ProductManagement.jsx
@@ -54,21 +54,23 @@ export const ProductManagement = () => {
                 required
               />
             </div>
-            <div className="w-full">
+            <div>
               <label
-                htmlFor="model"
+                htmlFor="category"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-white"
               >
-                Model
+                Category
               </label>
-              <input
-                type="text"
-                name="model"
-                id="model"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Product model"
-                required
-              />
+              <select
+                id="category"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              >
+                <option value="">Select category</option>
+                <option value="TV">Laptop Accessories</option>
+                <option value="PC">Laptop Peripheral</option>
+                <option value="GA">Replacement Parts</option>
+                <option value="PH">Cleaning & Maintenance</option>
+              </select>
             </div>
             <div className="w-full">
               <label
@@ -102,24 +104,7 @@ export const ProductManagement = () => {
                 required
               />
             </div>
-            <div>
-              <label
-                htmlFor="category"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-white"
-              >
-                Category
-              </label>
-              <select
-                id="category"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              >
-                <option value="">Select category</option>
-                <option value="TV">Laptop Accessories</option>
-                <option value="PC">Laptop Peripheral</option>
-                <option value="GA">Replacement Parts</option>
-                <option value="PH">Cleaning & Maintenance</option>
-              </select>
-            </div>
+            
             <div className="sm:col-span-2">
               <label
                 htmlFor="description"
