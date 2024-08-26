76e959edbd9255eca9beb320a03abb6069779c4f
Laibawaqar123 <laibawaqar72@gmail.com>
2024-08-26 18:56:17 +0500

    order button update

--- src/components/Dashboard/Orders.jsx
+++ src/components/Dashboard/Orders.jsx
@@ -180,9 +180,19 @@ export const OrderManagement2 = () => {
                     </div>
                   )}
                 </td>
-                <td className="px-2 py-1">
-                  <button onClick={handleUpdate} className="text-blue-500 hover:underline">Update</button>
-                  <button onClick={handleDelete} className="text-red-500 hover:underline ml-4">Delete</button>
+                <td className="px-6 py-3">
+                  <button
+                    onClick={() => handleUpdate()}
+                    className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                    Update
+                  </button>
+                  <button
+                    onClick={() => handleDelete()}
+                    className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
