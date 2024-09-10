ceb06a045228b9c3f545dcb38732745e98e2a499
Laibawaqar123 <laibawaqar72@gmail.com>
2024-09-10 23:05:24 +0500

    Svg Correction in dashboard

--- src/components/Dashboard/Forbidden.jsx
+++ src/components/Dashboard/Forbidden.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { TbLockSquareRoundedFilled } from "react-icons/tb";
 
 export const Forbidden = () => {
   const navigate = useNavigate();
@@ -12,19 +13,7 @@ export const Forbidden = () => {
         You do not have permission to view this page without the user's permission.
       </p>
       <div className="bg-red-600 p-6 rounded-full mb-6 animate-pulse">
-        <svg
-          className="w-[47px] h-[47px] text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z"
-            clipRule="evenodd"
-          />
-        </svg>
+      <TbLockSquareRoundedFilled />
       </div>
       <p className="text-lg md:text-xl text-gray-400 mb-10">
         It seems you don't have the required permissions to access this content.
