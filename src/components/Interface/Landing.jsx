ed4a3b1a61db24ee55e8a96547688e12c12252fd
engumairahmed <eng.umair.a@gmail.com>
2024-09-16 15:50:09 +0500

    Products on landing page

--- src/components/Interface/Landing.jsx
+++ src/components/Interface/Landing.jsx
@@ -87,22 +87,22 @@ function HomePage() {
 
                 <div className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all">
                   <div className="w-full h-[130px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
-                    <img src="https://readymadeui.com/images/watch1.webp" alt="product1"
+                    <img src="/products/adapter1.jpg" alt="product1"
                       className="h-full w-5/6 mx-auto block object-contain" />
                   </div>
                   <div className="text-center mt-4">
-                    <h3 className="text-sm font-bold text-gray-800">French Timex</h3>
+                    <h3 className="text-sm font-bold text-gray-800">Laptop Adapter</h3>
                     <h4 className="text-base text-blue-600 font-bold mt-2">$95.00</h4>
                   </div>
                 </div>
 
                 <div className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all">
                   <div className="w-full h-[130px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
-                    <img src="https://readymadeui.com/images/product14.webp" alt="product2"
+                    <img src="/products/adapter2.jpg" alt="product2"
                       className="h-full w-5/6 mx-auto block object-contain" />
                   </div>
                   <div className="text-center mt-4">
-                    <h3 className="text-sm font-bold text-gray-800">Echo Elegance</h3>
+                    <h3 className="text-sm font-bold text-gray-800">Laptop Adapter</h3>
                     <h4 className="text-base text-blue-600 font-bold mt-2">$20.00</h4>
                   </div>
                 </div>
@@ -120,22 +120,22 @@ function HomePage() {
 
                 <div className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all">
                   <div className="w-full h-[130px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
-                    <img src="https://readymadeui.com/images/watch4.webp" alt="product4"
+                    <img src="/products/dc_jack_1.jpg" alt="product4"
                       className="h-full w-5/6 mx-auto block object-contain" />
                   </div>
                   <div className="text-center mt-4">
-                    <h3 className="text-sm font-bold text-gray-800">Irish Cream Dream</h3>
+                    <h3 className="text-sm font-bold text-gray-800">Lenovo DC Jack</h3>
                     <h4 className="text-base text-blue-600 font-bold mt-2">$11.00</h4>
                   </div>
                 </div>
 
                 <div className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all">
                   <div className="w-full h-[130px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
-                    <img src="https://readymadeui.com/images/coffee7.webp" alt="product5"
+                    <img src="/products/keyboard2.jpg" alt="product5"
                       className="h-full w-5/6 mx-auto block object-contain" />
                   </div>
                   <div className="text-center mt-4">
-                    <h3 className="text-sm font-bold text-gray-800">Luxury desk clock</h3>
+                    <h3 className="text-sm font-bold text-gray-800">Laptop Keyboard</h3>
                     <h4 className="text-base text-blue-600 font-bold mt-2">$90.00</h4>
                   </div>
                 </div>
@@ -157,7 +157,7 @@ function HomePage() {
                       className="h-full w-5/6 mx-auto block object-contain" />
                   </div>
                   <div className="text-center mt-4">
-                    <h3 className="text-sm font-bold text-gray-800">Creative Wall Clock</h3>
+                    <h3 className="text-sm font-bold text-gray-800">Smart Watch</h3>
                     <h4 className="text-base text-blue-600 font-bold mt-2">$50.00</h4>
                   </div>
                 </div>
@@ -175,22 +175,22 @@ function HomePage() {
 
                 <div className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all">
                   <div className="w-full h-[130px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
-                    <img src="https://readymadeui.com/images/watch3.webp" alt="product1"
+                    <img src="/products/battery2.png" alt="product1"
                       className="h-full w-5/6 mx-auto block object-contain" />
                   </div>
                   <div className="text-center mt-4">
-                    <h3 className="text-sm font-bold text-gray-800">French Timex</h3>
+                    <h3 className="text-sm font-bold text-gray-800">Laptop Battery</h3>
                     <h4 className="text-base text-blue-600 font-bold mt-2">$95.00</h4>
                   </div>
                 </div>
 
                 <div className="bg-white p-3 cursor-pointer shadow-sm rounded-md hover:scale-[1.03] transition-all">
                   <div className="w-full h-[130px] overflow-hidden mx-auto aspect-w-16 aspect-h-8">
-                    <img src="https://readymadeui.com/images/product14.webp" alt="product2"
+                    <img src="/products/laptop_cpu_fan.jpg" alt="product2"
                       className="h-full w-5/6 mx-auto block object-contain" />
                   </div>
                   <div className="text-center mt-4">
-                    <h3 className="text-sm font-bold text-gray-800">Echo Elegance</h3>
+                    <h3 className="text-sm font-bold text-gray-800">Laptop CPU Fan</h3>
                     <h4 className="text-base text-blue-600 font-bold mt-2">$20.00</h4>
                   </div>
                 </div>
