a2c1e3f7d080d82d069c476f24a1e443db122122
Laibawaqar123 <laibawaqar72@gmail.com>
2024-09-26 17:39:41 +0500

    svg change of login reg

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -6,7 +6,8 @@ import * as Yup from "yup";
 import { ToastContainer, toast } from "react-toastify";
 import { TbEyeFilled } from "react-icons/tb"; 
 import { TbEyeClosed } from "react-icons/tb";
-// import 'react-toastify/dist/ReactToastify.css';
+import { MdEmail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 import Cookies from "js-cookie";
 
 function SignupForm() {
@@ -135,24 +136,8 @@ function SignupForm() {
                       onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
                     />
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="#bbb"
-                      stroke="#bbb"
-                      className="w-[18px] h-[18px] absolute right-2"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        cx="10"
-                        cy="7"
-                        r="6"
-                        data-original="#000000"
-                      ></circle>
-                      <path
-                        d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
-                        data-original="#000000"
-                      ></path>
-                    </svg>
+                   <FaUser  className="w-[18px] h-[18px] absolute right-2"
+                    style={{ color: "#bbb" }}  />
                   </div>
                   {formik.touched.name && formik.errors.name ? (
                     <div className="text-red-600 text-xs mt-1">
@@ -180,38 +165,10 @@ function SignupForm() {
                       onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
                     />
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="#bbb"
-                      stroke="#bbb"
-                      className="w-[18px] h-[18px] absolute right-2"
-                      viewBox="0 0 682.667 682.667"
-                    >
-                      <defs>
-                        <clipPath id="a" clipPathUnits="userSpaceOnUse">
-                          <path
-                            d="M0 512h512V0H0Z"
-                            data-original="#000000"
-                          ></path>
-                        </clipPath>
-                      </defs>
-                      <g
-                        clipPath="url(#a)"
-                        transform="matrix(1.33 0 0 -1.33 0 682.667)"
-                      >
-                        <path
-                          fill="none"
-                          strokeMiterlimit="10"
-                          strokeWidth="40"
-                          d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
-                          data-original="#000000"
-                        ></path>
-                        <path
-                          d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
-                          data-original="#000000"
-                        ></path>
-                      </g>
-                    </svg>
+                 <MdEmail
+                    className="w-[18px] h-[18px] absolute right-2"
+                    style={{ color: "#bbb" }} 
+                  />
                   </div>
                   {formik.touched.email && formik.errors.email ? (
                     <div className="text-red-600 text-xs mt-1">
--- src/components/Auth/login.jsx
+++ src/components/Auth/login.jsx
@@ -1,124 +1,125 @@
-import { React, useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { React, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Cookies from 'js-cookie';
-import * as jwt from 'jwt-decode'
-import { googleLogout, useGoogleLogin } from '@react-oauth/google';
-import { TbEyeFilled } from "react-icons/tb"; 
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Cookies from "js-cookie";
+import * as jwt from "jwt-decode";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import { TbEyeFilled } from "react-icons/tb";
+import { MdEmail } from "react-icons/md";
 import { TbEyeClosed } from "react-icons/tb";
-import { FcGoogle } from 'react-icons/fc';
+import { FcGoogle } from "react-icons/fc";
 
 function LoginForm() {
-
-  const URL = import.meta.env.VITE_URL
+  const URL = import.meta.env.VITE_URL;
 
   const [passwordsVisible, setPasswordsVisible] = useState(false);
 
   const [rememberMe, setRememberMe] = useState(false);
 
-  const [ user, setUser ] = useState([]);
-  const [ profile, setProfile ] = useState([]);
-
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
 
   const googleLogin = useGoogleLogin({
-      onSuccess: (codeResponse) => {
-        setUser(codeResponse)
-        console.log(codeResponse);
-        
-        const decoded=jwt.decode(codeResponse.access_token)
-        console.log(decoded);
-        
-        // Cookies.set("authToken", codeResponse.access_token, { expires: 1 })
-      },
-      onError: (error) => console.log('Login Failed:', error)
+    onSuccess: (codeResponse) => {
+      setUser(codeResponse);
+      console.log(codeResponse);
+
+      const decoded = jwt.decode(codeResponse.access_token);
+      console.log(decoded);
+
+      // Cookies.set("authToken", codeResponse.access_token, { expires: 1 })
+    },
+    onError: (error) => console.log("Login Failed:", error),
   });
 
-  const login = (values)=>{
+  const login = (values) => {
     axios
-        .post(`${URL}/login`, values)
-        .then((result) => {
-          const token = result.data.token;
-          Cookies.set( "authToken", token, rememberMe ? { expires: 7 } : {} )
+      .post(`${URL}/login`, values)
+      .then((result) => {
+        const token = result.data.token;
+        Cookies.set("authToken", token, rememberMe ? { expires: 7 } : {});
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 500);
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          toast.error(error.response.data.msg, { theme: "dark" });
+          formik.setErrors({
+            ...formik.errors,
+            email: error.response.data.msg,
+          });
+        } else if (error.response.status === 401) {
+          console.log(error.request);
+          toast.error(error.response.data.msg, { theme: "dark" });
+        } else if (error.response.status === 403) {
+          toast.error(error.response.data.msg, { theme: "dark" });
           setTimeout(() => {
-            navigate("/dashboard");
-          }, 500);
-        })
-        .catch((error) => {
-          if (error.response.status === 404) {
-            toast.error(error.response.data.msg, { theme: 'dark' });
-            formik.setErrors({ ...formik.errors, email: error.response.data.msg });
-          } else if (error.response.status === 401) {
-            console.log(error.request);
-            toast.error(error.response.data.msg, { theme: 'dark' });
-          } else if (error.response.status === 403) {
-            toast.error(error.response.data.msg, { theme: 'dark' });
-            setTimeout(() => {
-              navigate('/email-verification')
-            }, 1000);
-          } else {
-            console.log('Error', error.message);
-            toast.error('Something went wrong.', { theme: 'dark' });
-          }
-        });
-  }
-
+            navigate("/email-verification");
+          }, 1000);
+        } else {
+          console.log("Error", error.message);
+          toast.error("Something went wrong.", { theme: "dark" });
+        }
+      });
+  };
 
   const navigate = useNavigate();
 
   const validationSchema = Yup.object({
     email: Yup.string()
-      .email('Invalid email address')
-      .required('Email is required'),
-    password: Yup.string()
-      .required('Password is required'),
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string().required("Password is required"),
   });
 
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
-      email: '',
-      password: ''
+      email: "",
+      password: "",
     },
     validationSchema,
     onSubmit: (values) => {
       login(values);
-    }
+    },
   });
 
   const logOut = () => {
     googleLogout();
     setProfile(null);
-};
+  };
 
   useEffect(() => {
-    if (Cookies.get('authToken')) {
-      navigate('/dashboard');
+    if (Cookies.get("authToken")) {
+      navigate("/dashboard");
     }
-      if (user) {
-        console.log(user);
-        
-          axios
-              .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                  headers: {
-                      Authorization: `Bearer ${user.access_token}`,
-                      Accept: 'application/json'
-                  }
-              })
-              .then((res) => {
-                  setProfile(res.data);
-              })
-              .catch((err) => console.log(err));
-      }
-  },[user])
+    if (user) {
+      console.log(user);
 
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
 
   return (
     <>
-
       <div className="font-[sans-serif] bg-gray-900 md:h-screen">
         <div className="grid md:grid-cols-2 items-center gap-8 h-full">
           <div className="max-md:order-1 p-4 image-container">
@@ -130,93 +131,123 @@ function LoginForm() {
           </div>
 
           <div className="flex items-center md:p-8 p-6 bg-white md:rounded-tl-[55px] md:rounded-bl-[55px] h-full">
-            <form className="max-w-lg w-full mx-auto" onSubmit={formik.handleSubmit}>
-
-
-              <div id='sign' className="mb-20 flex flex-col items-center mb-6">
-                <div className='bg-black w-22 border-3 rounded-lg flex justify-center'>
-                  <Link to={'/'}>
-                    <img src="/Logo.png" alt="TradeVista" className='w-20' />
+            <form
+              className="max-w-lg w-full mx-auto"
+              onSubmit={formik.handleSubmit}
+            >
+              <div id="sign" className="mb-20 flex flex-col items-center mb-6">
+                <div className="bg-black w-22 border-3 rounded-lg flex justify-center">
+                  <Link to={"/"}>
+                    <img src="/Logo.png" alt="TradeVista" className="w-20" />
                   </Link>
                 </div>
                 <h3 className="text-blue-800 text-4xl font-extrabold">Login</h3>
-                <p className="text-gray-800 text-sm mt-4 ">Don't have an account ? <Link className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap" to={'/register'}>Register here</Link></p>
+                <p className="text-gray-800 text-sm mt-4 ">
+                  Don't have an account ?{" "}
+                  <Link
+                    className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap"
+                    to={"/register"}
+                  >
+                    Register here
+                  </Link>
+                </p>
               </div>
 
               <div>
-                <label className="text-gray-800 text-xs block mb-2">Email</label>
+                <label className="text-gray-800 text-xs block mb-2">
+                  Email
+                </label>
                 <div className="relative flex items-center">
-                  <input name="email" type="email" autoComplete='on' className="w-full text-sm border-2 border-gray-300 rounded-lg outline-none focus:border-gray-800 px-2 py-3" placeholder="Email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="#bbb" stroke="#bbb" className="w-[18px] h-[18px] absolute right-2" viewBox="0 0 682.667 682.667">
-                    <defs>
-                      <clipPath id="a" clipPathUnits="userSpaceOnUse">
-                        <path d="M0 512h512V0H0Z" data-original="#000000"></path>
-                      </clipPath>
-                    </defs>
-                    <g clipPath="url(#a)" transform="matrix(1.33 0 0 -1.33 0 682.667)">
-                      <path fill="none" strokeMiterlimit="10" strokeWidth="40" d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z" data-original="#000000"></path>
-                      <path d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z" data-original="#000000"></path>
-                    </g>
-                  </svg>
+                  <input
+                    name="email"
+                    type="email"
+                    autoComplete="on"
+                    className="w-full text-sm border-2 border-gray-300 rounded-lg outline-none focus:border-gray-800 px-2 py-3"
+                    placeholder="Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  <MdEmail
+                    className="w-[18px] h-[18px] absolute right-2"
+                    style={{ color: "#bbb" }} 
+                  />
                 </div>
 
                 {formik.touched.email && formik.errors.email ? (
-                  <div className="text-red-600 text-xs mt-1">{formik.errors.email}</div>
+                  <div className="text-red-600 text-xs mt-1">
+                    {formik.errors.email}
+                  </div>
                 ) : null}
               </div>
 
               <div className="mt-4">
-                  <label className="text-gray-800 text-xs block">
-                    Password
-                  </label>
-                  <div className="relative flex items-center">
-                    <input
-                      name="password"
-                      type={passwordsVisible ? "text" : "password"}
-                      className="w-full text-sm border-2 rounded-lg border-gray-300 focus:border-gray-800 px-2 py-3 outline-none"
-                      placeholder="Password"
-                      value={formik.values.password}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                    />
-                    {passwordsVisible ? (
+                <label className="text-gray-800 text-xs block">Password</label>
+                <div className="relative flex items-center">
+                  <input
+                    name="password"
+                    type={passwordsVisible ? "text" : "password"}
+                    className="w-full text-sm border-2 rounded-lg border-gray-300 focus:border-gray-800 px-2 py-3 outline-none"
+                    placeholder="Password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                  {passwordsVisible ? (
                     <TbEyeFilled
-                        className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
-                        onClick={() => setPasswordsVisible(!passwordsVisible)}
-                        style={{ color: "#9CA3AF" }} // Set the color to gray
-                      />
-                    ) : (
-                      <TbEyeClosed
-                        className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
-                        onClick={() => setPasswordsVisible(!passwordsVisible)}
-                        style={{ color: "#9CA3AF" }} // Set the color to gray
-                      />
-                    )}
-                  </div>
-                  {formik.touched.password && formik.errors.password ? (
-                    <div className="text-red-600 text-xs mt-1">
-                      {formik.errors.password}
-                    </div>
-                  ) : null}
+                      className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                      onClick={() => setPasswordsVisible(!passwordsVisible)}
+                      style={{ color: "#9CA3AF" }} // Set the color to gray
+                    />
+                  ) : (
+                    <TbEyeClosed
+                      className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                      onClick={() => setPasswordsVisible(!passwordsVisible)}
+                      style={{ color: "#9CA3AF" }} // Set the color to gray
+                    />
+                  )}
                 </div>
+                {formik.touched.password && formik.errors.password ? (
+                  <div className="text-red-600 text-xs mt-1">
+                    {formik.errors.password}
+                  </div>
+                ) : null}
+              </div>
 
               <div className="flex flex-wrap items-center justify-between gap-4 mt-6">
                 <div className="flex items-center">
-                  <input id="remember-me" name="remember-me" type="checkbox" checked={rememberMe} onChange={() => setRememberMe(!rememberMe)} className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded" />
-                  <label htmlFor="remember-me" className="text-gray-800 ml-3 block text-sm">
+                  <input
+                    id="remember-me"
+                    name="remember-me"
+                    type="checkbox"
+                    checked={rememberMe}
+                    onChange={() => setRememberMe(!rememberMe)}
+                    className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                  />
+                  <label
+                    htmlFor="remember-me"
+                    className="text-gray-800 ml-3 block text-sm"
+                  >
                     Remember me
                   </label>
                 </div>
                 <div>
-                  <Link className="text-blue-600 font-semibold text-sm hover:underline" to={'/forgot-password'}>
-                    Forgot Password? </Link>
-
+                  <Link
+                    className="text-blue-600 font-semibold text-sm hover:underline"
+                    to={"/forgot-password"}
+                  >
+                    Forgot Password?{" "}
+                  </Link>
                 </div>
               </div>
 
               <div className="mt-12">
-                <button type="submit" className="w-full py-3 px-6 text-sm font-semibold tracking-wider rounded-full text-white bg-blue-800 bg-[#00808] focus:outline-none">
-                  Login</button>
+                <button
+                  type="submit"
+                  className="w-full py-3 px-6 text-sm font-semibold tracking-wider rounded-full text-white bg-blue-800 bg-[#00808] focus:outline-none"
+                >
+                  Login
+                </button>
               </div>
 
               <div className="my-6 flex items-center gap-4">
@@ -225,22 +256,20 @@ function LoginForm() {
                 <hr className="w-full border-gray-300" />
               </div>
 
-              <button type="button" className="w-full flex items-center justify-center gap-4 py-3 px-6 text-sm font-semibold tracking-wider text-gray-800 border border-gray-300 rounded-full bg-gray-50 hover:bg-gray-100 focus:outline-none"
-               onClick={() => googleLogin()}>
-              <FcGoogle size={22}/>
-              Continue with google
-            </button>
+              <button
+                type="button"
+                className="w-full flex items-center justify-center gap-4 py-3 px-6 text-sm font-semibold tracking-wider text-gray-800 border border-gray-300 rounded-full bg-gray-50 hover:bg-gray-100 focus:outline-none"
+                onClick={() => googleLogin()}
+              >
+                <FcGoogle size={22} />
+                Continue with google
+              </button>
               <ToastContainer />
             </form>
           </div>
         </div>
       </div>
-
-
-
-
     </>
-
   );
 }
 
