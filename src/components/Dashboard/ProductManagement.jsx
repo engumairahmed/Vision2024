c23ef38892fbc84faa2dde33e8e7ffcb06ed2ad0
Laibawaqar123 <laibawaqar72@gmail.com>
2024-11-02 03:45:52 +0500

    prod add form responsive

--- src/components/Dashboard/ProductManagement.jsx
+++ src/components/Dashboard/ProductManagement.jsx
@@ -3,36 +3,32 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { toast } from "react-toastify";
-import { FaImage , FaCloudUploadAlt } from "react-icons/fa";
+import { FaImage, FaCloudUploadAlt } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import Cookies from 'js-cookie';
-import * as jwt from 'jwt-decode';
+import Cookies from "js-cookie";
+import * as jwt from "jwt-decode";
 import { useAuth } from "../Auth/AuthContext";
 
-
 export const ProductManagement = ({ user }) => {
-
   const { getUserId } = useAuth();
   const userId = getUserId();
 
   const navigate = useNavigate();
 
-
   const [authToken, setToken] = useState();
   const [decodedToken, setDecodedToken] = useState();
 
-  const token = Cookies.get('authToken');
+  const token = Cookies.get("authToken");
 
   const deco = jwt.jwtDecode(token);
 
-  const newId = deco.id
-
+  const newId = deco.id;
 
   const [User, setUser] = useState(user);
   const [imagePreview, setImagePreview] = useState(null);
   const [id, setUserId] = useState(newId);
 
-  const viteURL = import.meta.env.VITE_URL
+  const viteURL = import.meta.env.VITE_URL;
 
   const validationSchema = Yup.object({
     name: Yup.string().required("Product name is required"),
@@ -64,60 +60,56 @@ export const ProductManagement = ({ user }) => {
       quantity: "",
       description: "",
       image: "",
-      id:id,
+      id: id,
     },
     validationSchema: validationSchema,
     onSubmit: (values, { resetForm }) => {
       const formData = new FormData();
-      formData.append('name', values.name);
-      formData.append('brand', values.brand);
-      formData.append('category', values.category);
-      formData.append('price', values.price);
-      formData.append('quantity', values.quantity);
-      formData.append('description', values.description);
-      formData.append('image', values.image);
-      formData.append('id', values.id);
-      setImagePreview(null)
+      formData.append("name", values.name);
+      formData.append("brand", values.brand);
+      formData.append("category", values.category);
+      formData.append("price", values.price);
+      formData.append("quantity", values.quantity);
+      formData.append("description", values.description);
+      formData.append("image", values.image);
+      formData.append("id", values.id);
+      setImagePreview(null);
 
-
-      axios.post(`${viteURL}/add-product`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
+      axios
+        .post(`${viteURL}/add-product`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
         .then(() => {
-
           toast.info("Product added!");
           resetForm();
         })
         .catch((error) => {
           toast.error(error.message);
-        })
-    }
-
+        });
+    },
   });
 
-
-
   useEffect(() => {
-    const token = Cookies.get('authToken');
+    const token = Cookies.get("authToken");
     if (token) {
       const decoded = jwt.jwtDecode(token);
       setDecodedToken(decoded);
       setToken(token);
-      setUserId(decoded.id);     
+      setUserId(decoded.id);
     }
-  },[token]);
+  }, [token]);
 
   return (
-    <div className="product-container p-8 w-100 mt-10 ">
-      <section className="bg-gray-500 dark:bg-gray-900 p-8 rounded-lg shadow-md">
-        <h2 className="mb-4 text-3xl font-bold text-black">
+    <div className="product-container p-4 sm:p-8 w-full mt-4 sm:mt-10">
+      <section className="bg-gray-500 dark:bg-gray-900 p-6 sm:p-8 rounded-lg shadow-md">
+        <h2 className="mb-4 text-2xl sm:text-3xl font-bold text-black">
           Add Products
         </h2>
         <form onSubmit={formik.handleSubmit} encType="multipart/form-data">
           <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-            <div className="sm:col-span-2">
+            <div className="col-span-2 sm:col-span-2">
               <label
                 htmlFor="name"
                 className="block mb-2 text-sm font-medium dark:text-white text-white"
@@ -267,82 +259,67 @@ export const ProductManagement = ({ user }) => {
               ) : null}
             </div>
 
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="image"
-                className="block mb-2 text-xl text-center font-medium text-gray-100 dark:text-white"
-              >
-                Product Image
-              </label>
-
-              {/* Container for both image preview and upload button */}
-              <div className="flex items-center justify-center p-6 border-gray-300 border-dashed rounded-lg dark:border-gray-600 bg-gray-500 dark:bg-gray-700 h-48 space-x-6">
-                {/* Preview Image Section */}
-                <div className="flex items-center justify-center bg-gray-300 w-32 h-32 border-2 border-gray-300 border-solid rounded-lg">
-                  {imagePreview ? (
-                    <img
-                      src={imagePreview}
-                      alt="Preview"
-                      className="w-full h-full object-cover rounded-lg"
-                    />
-                  ) : (
-                    <span className="text-gray-500 dark:text-gray-400 text-sm">
-                      <FaImage className="w-8 h-8"/>
-                    </span>
-                  )}
-                </div>
-
-                {/* Upload Button Section */}
-                <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-500 bg-gray-300 h-32 w-32 rounded-lg">
-                  <input
-                    type="file"
-                    name="image"
-                    id="image"
-                    className="hidden"
-                    onChange={(event) => {
-                      const file = event.currentTarget.files[0];
-                      formik.setFieldValue("image", file);
-                      setImagePreview(URL.createObjectURL(file));
-                    }}
+            <div className="col-span-2 sm:col-span-2 flex justify-center space-x-6 mb-4">
+             
+              <div className="flex items-center justify-center bg-gray-300 w-full sm:w-32 h-32 border-2 border-gray-300 rounded-lg">
+                {imagePreview ? (
+                  <img
+                    src={imagePreview}
+                    alt="Preview"
+                    className="w-full h-full object-cover rounded-lg"
                   />
-                  <label
-                    htmlFor="image"
-                    className="flex flex-col items-center cursor-pointer"
-                  >
-                    <FaCloudUploadAlt className="w-8 h-8" color="#69707f"/>
-                    <span className="text-gray-500 dark:text-gray-400 text-sm text-center">
-                      Click to upload photo
-                    </span>
-                    <span className="text-sm text-gray-400 dark:text-gray-500">
-                      Max size: 5MB
-                    </span>
-                  </label>
-                  {formik.values.image && (
-                    <span className="text-sm text-gray-600 mt-2 dark:text-gray-400">
-                      {formik.values.image.name}
-                    </span>
-                  )}
-                </div>
+                ) : (
+                  <span className="text-gray-500 dark:text-gray-400 text-sm">
+                    <FaImage className="w-8 h-8" />
+                  </span>
+                )}
               </div>
 
-              {/* Error message */}
-              {formik.touched.image && formik.errors.image ? (
-                <div className="text-red-500 text-sm mt-2">
-                  {formik.errors.image}
-                </div>
-              ) : null}
+              <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-500 bg-gray-300 w-full sm:w-32 h-32 rounded-lg">
+                <input
+                  type="file"
+                  name="image"
+                  id="image"
+                  className="hidden"
+                  onChange={(event) => {
+                    const file = event.currentTarget.files[0];
+                    formik.setFieldValue("image", file);
+                    setImagePreview(URL.createObjectURL(file));
+                  }}
+                />
+                <label
+                  htmlFor="image"
+                  className="flex flex-col items-center cursor-pointer"
+                >
+                  <FaCloudUploadAlt className="w-8 h-8" color="#69707f" />
+                  <span className="text-gray-500 dark:text-gray-400 text-sm text-center">
+                    Click to upload photo
+                  </span>
+                  <span className="text-sm text-gray-400 dark:text-gray-500">
+                    Max size: 5MB
+                  </span>
+                </label>
+                {formik.values.image && (
+                  <span className="text-sm text-gray-600 mt-2 dark:text-gray-400">
+                    {formik.values.image.name}
+                  </span>
+                )}
+              </div>
             </div>
-
+            {formik.touched.image && formik.errors.image ? (
+              <div className="text-red-500 text-sm mt-2">
+                {formik.errors.image}
+              </div>
+            ) : null}
           </div>
-          <div className="flex items-center justify-center">
-          <button
-            type="submit"
-            className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-          >
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Add Product
-            </span>
-          </button>
+
+          <div className="flex items-center justify-center mt-4">
+            <button
+              type="submit"
+              className="px-4 py-2 text-white bg-blue-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+            >
+              Submit
+            </button>
           </div>
         </form>
       </section>
